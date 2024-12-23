d8fe989be2410af6da1cb72c66281c8fe5f21b29
FIXED-TERM HE Zhuolin (CR/RIX3-AP) <hez4sgh@SGH-C-003YN.sgh.apac.bosch.com>
2024-12-23 11:36:24 +0800

    init sh file and upgrade gitignore

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 CUDA_VISIBLE_DEVICES=0 \
 nohup python main.py \
 --eval \
---ckpt_path ./logs/CGFormer-Efficient-Swin-KITTI360/tensorboard/version_0/checkpoints/best.ckpt \
---config_path configs/CGFormer-Efficient-Swin-KITTI360.py \
---log_folder version1 \
+--ckpt_path ./logs/CGFormer-Efficient-Swin-KITTI360-unified/tensorboard/version_4/checkpoints/best.ckpt \
+--config_path configs/CGFormer-Efficient-Swin-KITTI360-unified.py \
+--log_folder version4 \
 --seed 7240 \
 --log_every_n_steps 100 \
-> CGFormer-Efficient-Swin-SemanticKITTI-Eval.log 2>&1 &
+> CCGFormer-Efficient-Swin-KITTI360-unified-Eval.log 2>&1 &
--- train.sh
+++ train.sh
@@ -1,8 +1,8 @@
 CUDA_VISIBLE_DEVICES=0,1,2,3 \
 nohup python main.py \
---config_path configs/CGFormer-Efficient-Swin-SemanticKITTI-Pretrain.py \
---log_folder CGFormer-Efficient-Swin-SemanticKITTI-Pretrain \
+--config_path configs/CGFormer-Efficient-Swin-KITTI360-unified.py \
+--log_folder CGFormer-Efficient-Swin-KITTI360-unified \
 --seed 7240 \
 --pretrain \
 --log_every_n_steps 100 \
-> CGFormer-Efficient-Swin-SemanticKITTI-Pretrain.log 2>&1 &
+> CGFormer-Efficient-Swin-KITTI360-unified.log 2>&1 &
