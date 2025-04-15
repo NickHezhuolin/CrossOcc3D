b00e983f0e1a7a4f5b9bf1c1962cb8abe31bf584
FIXED-TERM HE Zhuolin (CR/RIX3-AP) <hez4sgh@SGH-C-003YN.sgh.apac.bosch.com>
2025-04-15 23:00:27 +0800

    update gs-resnet101-config

--- configs/SimperGaussianPretrain-Resnet101.py
+++ configs/SimperGaussianPretrain-Resnet101.py
@@ -266,3 +266,25 @@ lr_scheduler = dict(
     interval="step",
     frequency=1
 )
+
+# GSPretrain setup
+# total_epochs = 12
+
+# optimizer = dict(
+#     type="AdamW",
+#     lr=2e-4,
+#     paramwise_cfg=dict(
+#         custom_keys={
+#             "img_backbone": dict(lr_mult=0.1),
+#         }
+#     ),
+#     weight_decay=0.01,
+# )
+
+# lr_config = dict(
+#     policy="CosineAnnealing",
+#     warmup="linear",
+#     warmup_iters=500,
+#     warmup_ratio=1.0 / 3,
+#     min_lr_ratio=1e-3,
+# )
